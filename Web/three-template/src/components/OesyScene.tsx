e08d0ce318c3539f44b0ffd2f7d2973547fa4abb
exy <teslawiktor@gmail.com>
2026-03-26 16:42:51 +0100

    VectorByWiktor

--- Web/three-template/src/components/OesyScene.tsx
+++ Web/three-template/src/components/OesyScene.tsx
@@ -10,10 +10,12 @@ export default function OesyScene() {
       style={{ width: "100%", height: "100%" }}
       gl={{ alpha: false }}
     >
-      <color attach="background" args={["#080808"]} />
-      <ambientLight intensity={0.2} />
-      <pointLight position={[5, 5, 5]} intensity={6} color="#ffffff" />
-      <pointLight position={[-5, 3, 2]} intensity={4} color="#8866ff" />
+      <color attach="background" args={["#0a0010"]} />
+      <ambientLight intensity={0.3} color="#6633cc" />
+      <pointLight position={[5, 5, 5]} intensity={5} color="#ffffff" />
+      <pointLight position={[-5, 3, 2]} intensity={10} color="#8833ff" />
+      <pointLight position={[3, -3, 2]} intensity={8} color="#aa44ff" />
+      <pointLight position={[0, 5, -2]} intensity={6} color="#cc66ff" />
       <pointLight position={[0, -5, 3]} intensity={3} color="#ff4488" />
       <OrbitControls enableDamping target={[0, 0, 0]} />
       <Suspense fallback={null}>
